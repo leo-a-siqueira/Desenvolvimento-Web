fabd7ebb4264a4acb288f357776d554db3c3b692
leo-a-siqueira <leoapss151@gmail.com>
2024-04-20 12:00:47 -0300

    upload aula01

---
+++ Aulas/Aula01/js/variaveis.js
@@ -0,0 +1,20 @@
+var nome = "Fulano" // string
+var nome = String("Ciclano"); // string
+
+var numero1 = 5
+var numero2 = 5.5
+var numero3 = Number("10")
+var numero4 = Number(10)
+var numero5 = parseInt(5.3)
+var numero6 = parseFloat()
+
+var ehMaiorDeIdade = true
+var ehVermelho = false
+
+var naoEhDefinido = undefined
+var vazio = null
+var naoEhNumero = NaN
+
+console.log(naoEhDefinido)
+console.log(vazio)
+console.log(naoEhNumero)
