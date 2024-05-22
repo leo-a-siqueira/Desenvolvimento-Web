48316bc88f5624bbed22dabf32f7e1ab7f911e7b
leo-a-siqueira <leoapss151@gmail.com>
2024-05-22 19:51:04 -0300

    Lista 5

---
+++ Listas/lista05/ex01/js/script.js
@@ -0,0 +1,6 @@
+var nome = prompt('Digite seu nome:')
+var idade = Number(prompt('Digite sua idade:'))
+var emprego = prompt('Digite seu emprego atual:')
+var endereco = prompt('Digite seu endereco:')
+
+alert(`Olá ${nome} \nVocê tem ${idade}\nSeu emprego atual é ${emprego} \nVocê mora no endereço ${endereco}`)
---
+++ Listas/lista05/ex02/ex02.js
@@ -0,0 +1,6 @@
+var nome = "Fulano"
+var idade = 25
+var emprego = "Desenvolvedor"
+var endereco = "Rua 26 de Março, número 500"
+
+console.log(`Olá ${nome} \nVocê tem ${idade}\nSeu emprego atual é ${emprego} \nVocê mora no endereço ${endereco}`)
