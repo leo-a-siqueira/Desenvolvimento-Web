dfb7c0c5e2e764d2645f0f06a9a061524d4f8c77
leo-a-siqueira <leoapss151@gmail.com>
2024-05-29 21:07:21 -0300

    aula6

---
+++ Aulas/Aula06/js/objetos.js
@@ -0,0 +1,23 @@
+// Ojetos literais em Javascript
+
+var pessoa = {
+    nome: 'Leo',
+    idade: 30,
+    endereco: 'Rua XPTO',
+    numeroCasa: 550,
+    cargo: 'Front-end Pleno',
+    conjugue: 'Aléxia',
+    telefone: ["11970511093" , "11955446632"],
+    filhos: [
+        {
+            nome: 'Luis',
+            idade: 2
+        }
+        {
+            nome: 'Luciana',
+            idade: 1
+        }
+    ]
+}
+
+console.log(pessoa)
